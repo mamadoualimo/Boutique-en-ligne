6d889780c1c6043aed973a34a9343db7dcc5479a
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-03-04 11:47:56 +0100

    la partie produits, panier du site

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Search;
+use App\Entity\Product;
+use App\Form\SearchType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+
+    private $productRepository;
+
+    public function __construct(ProductRepository $productRopository)
+    {
+
+        $this->productRepository = $productRopository;
+    }
+
+    /**
+     * @Route("/nos-produits", name="products")
+     */
+    public function index(Request $request): Response
+    {
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $this->productRepository->findWithSearch($search);
+        } else {
+            $products = $this->productRepository->findAll();
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/produit/{slug}", name="product")
+     */
+    public function show($slug): Response
+    {
+        $product =  $this->productRepository->findOneBySlug($slug);
+
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+}
